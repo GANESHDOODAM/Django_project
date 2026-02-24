aa7280cb46580b0e869b23c11ca2a5a32b4d2d96
GANESHDOODAM <doodamganesh123@gmail.com>
2026-02-24 15:41:49 +0530

    added home and about views, updated urls.py to include these views

--- firstProject/views.py
+++ firstProject/views.py
@@ -1,5 +1,5 @@
-from views import render
-from django.http import HttpResponse, httpResponse
+from django.shortcuts import render
+from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("Hello, World!")
